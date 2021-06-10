bd57b2bc002e54bee4ecec16f0195f45f2755108
Juan Arredondo <juan.arredondo.c@usach.cl>
2021-06-09 22:22:09 -0400

    Parte 1 - lab 2

---
+++ lab 2/parte1.m
@@ -0,0 +1,134 @@
+% Primera parte de laboratorio
+
+% Se explica como obtener los gráficos de las respuestas de lazo abierto 
+% y cerrado de las siguientes funciones
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%% Funcion 1 %%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% 6*y'(t) + 2*y(t) = 8*u'(t)
+% Despues se utilizada la tranformada de Laplace, dejando la función
+% expresada en el parametro s. Realizando una serie de calculos la funcion
+% de tranferencia quedaría:
+% Y(s) = 4*s*u(s) / (3*s+1)
+
+% Definimos el numerador y denominador de la función de transferencia
+numerador1= [4,0];
+denominador1= [3,1];
+% Se utiliza tf para definir la función de trasnferencia
+s1 = tf(numerador1,denominador1);
+
+% Para obtener los polos y zeros de la funcion
+[s1_z, s1_p, s1_k] = zpkdata(s1);
+% Para obtener el tiempo de estabilizacion se utiliza stepinfo()
+s1_estab = stepinfo(s1);
+
+% Gráfico de lazo abierto
+figure(1)
+step(s1);
+title("Función de transferencia 1: Lazo Abierto");
+grid on;
+
+
+
+%Utilizando la función 'feedback' se calcula el lazo cerrado
+%Se coloca 1 como H2 porque el feedback entre dos funciones debe ser el
+%mismo que si fuera solo una función (el cual es este caso).
+s1_cerrado = feedback(s1,1,-1);
+
+
+% Para obtener los polos, zeros y ganancia 
+% de la funcion se ocupa la función zpkdata.
+[s1_cerrado_z, s1_cerrado_p, s1_cerrado_k] = zpkdata(s1_cerrado);
+
+% Para obtener el tiempo de estabilizacion se utiliza stepinfo()
+s1_cerrado_estab = stepinfo(s1_cerrado);
+
+%Gráfico de lazo cerrado
+figure(2)
+step(s1_cerrado);
+title("Función de transferencia 1: Lazo Cerrado");
+grid on
+
+
+% Grafico de polos y zeros lazo abierto y cerrado
+figure(3)
+pzmap(s1,"b", s1_cerrado,"r")
+title("Ceros y polos de función de transferencia 1");
+legend("abierto", "cerrado")
+grid on;
+
+fprintf('Restulados funcion 1\n\n')
+fprintf('Lazo abierto\n')
+fprintf('Ganancia estatica: %d\n', s1_k)
+fprintf('Tiempo de estabilizacion: %d\n', s1_estab.SettlingTime)
+fprintf('Ceros: %d\n', s1_z{1})
+fprintf('Polos: %d\n', s1_p{1})
+
+fprintf('\nLazo cerrado\n')
+fprintf('Ganancia estatica: %d\n', s1_cerrado_k)
+fprintf('Tiempo de estabilizacion: %d\n', s1_cerrado_estab.SettlingTime)
+fprintf('Ceros: %d\n', s1_cerrado_z{1})
+fprintf('Polos: %d\n', s1_cerrado_p{1})
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%% Funcion 2 %%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%y''(t) + 6*y'(t) + 3*y(t) = 5*u''(t) + 7*u'(t) + u(t)
+%Despues se utilizada la tranformada de Laplace, dejando la función
+%expresada en el parametro s. Realizando una serie de calculos la funcion
+%de tranferencia quedaría:
+%Y(s) = (5*s² + 7*s + 1) / (s² + 6*s + 3)
+
+%Definimos el numerador y denominador de la función de transferencia
+numerador2= [5,7,1];
+denominador2= [1,6,3];
+%Se utiliza tf para definir la función de trasnferencia
+s2 = tf(numerador2,denominador2);
+
+% Para obtener los polos y zeros de la funcion
+[s2_z, s2_p ,s2_k] = zpkdata(s2);
+
+% Para obtener el tiempo de estabilizacion se utiliza stepinfo()
+s2_estab = stepinfo(s2);
+
+%Gráfico de lazo abierto
+figure(4)
+step(s2)
+title("Función de transferencia 2: Lazo Abierto");
+grid on
+
+s2_cerrado = feedback(s2,1,-1);
+
+% Para obtener los polos y zeros de la funcion
+[s2_cerrado_z, s2_cerrado_p , s2_cerrado_k] = zpkdata(s2_cerrado);
+
+% Para obtener el tiempo de estabilizacion se utiliza stepinfo()
+s2_cerrado_estab = stepinfo(s2_cerrado);
+
+%Gráfico de lazo cerrado
+figure(5);
+step(s2_cerrado);
+title("Función de transferencia 2: Lazo Cerrado");
+grid on
+
+% Grafico de polos y zeros lazo abierto y cerrado
+figure(6)
+pzmap(s2,"b", s2_cerrado,"r")
+title("Ceros y polos de función de transferencia 2");
+legend("abierto", "cerrado")
+grid on;
+
+fprintf('Restulados funcion 2\n\n')
+fprintf('Lazo abierto\n')
+fprintf('Ganancia estatica: %d\n', s2_k)
+fprintf('Tiempo de estabilizacion: %d\n', s2_estab.SettlingTime)
+fprintf('Ceros: %d\n', s2_z{1})
+fprintf('Polos: %d\n', s2_p{1})
+
+fprintf('\nLazo cerrado\n')
+fprintf('Ganancia estatica: %d\n', s2_cerrado_k)
+fprintf('Tiempo de estabilizacion: %d\n', s2_cerrado_estab.SettlingTime)
+fprintf('Ceros: %d\n', s2_cerrado_z{1})
+fprintf('Polos: %d\n', s2_cerrado_p{1})
